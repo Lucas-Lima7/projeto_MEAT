18b8567bd3dc9649610410701bba5765c26a5daa
Lucas Lima <lucaslimaandrade7@gmail.com>
2017-08-08 16:45:02 -0300

    Adicionando componente em /shared

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from '../shared/radio/radio-option.model'
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+	paymentOptions: RadioOption[] = [
+		{label: 'DINHEIRO', value: 'DIN'},
+		{label: 'CARTAO DEBITO', value: 'DEB'},
+		{label: 'CARTAO REFEIÇÃO', value: 'REF'}
+	]
 
   constructor() { }
 
