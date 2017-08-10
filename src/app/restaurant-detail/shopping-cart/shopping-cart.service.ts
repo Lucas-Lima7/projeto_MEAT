21c997e57d5e8dda3e1144f1d38942fd43a3739b
Lucas Lima <lucaslimaandrade7@gmail.com>
2017-08-10 17:37:04 -0300

    Adicionando componente order-items

--- src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -13,9 +13,20 @@ export class ShoppingCartService{
 	addItem(item:MenuItem){
 		let foundItem = this.items.find((mItem)=> mItem.menuItem.id === item.id)
 		if(foundItem){
-			foundItem.quantity = foundItem.quantity + 1
+			this.aumentarQtd(foundItem)
 		}else{
 			this.items.push(new CartItem(item))
+		}
+	}
+
+	aumentarQtd(item: CartItem){
+		item.quantity = item.quantity + 1
+	}
+
+	diminuirQtd(item: CartItem){
+		item.quantity = item.quantity - 1
+		if(item.quantity === 0){
+			this.removeItem(item)
 		}
 	}
 
